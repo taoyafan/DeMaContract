d67e6161dd67449df17b7a7b10d60ced1a0d9643
Tao Yafan <tao_yafan@qq.com>
2021-05-10 22:49:22 +0800

    Add a random address when deploy bank.

--- migrations/2_deploy_bank.js
+++ migrations/2_deploy_bank.js
@@ -7,7 +7,7 @@ module.exports = function (deployer, network, accounts) {
 
     deployer.deploy(TripleSlopeModel);
     deployer.deploy(BankConfig);
-    deployer.deploy(Bank);
+    deployer.deploy(Bank, "0x337610d27c682E347C9cD60BD4b3b107C9d34dDd");
     if(network == 'development') {
         const USDT = artifacts.require("ERC20Token");
         deployer.deploy(USDT, "USDT", "USDT", BigNumber(1000e18));
