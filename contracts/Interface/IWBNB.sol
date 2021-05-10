25896b9d624493c17d0ae63e31439df93fe9c884
Tao Yafan <tao_yafan@qq.com>
2021-05-10 22:49:41 +0800

    Add IWBNB functions.

--- contracts/Interface/IWBNB.sol
+++ contracts/Interface/IWBNB.sol
@@ -1,9 +1,13 @@
 pragma solidity >=0.5.0 <0.8.0;
 
 interface IWBNB {
-    function deposit() external payable;
+    function balanceOf(address user) external returns (uint);
+
+    function approve(address to, uint value) external returns (bool);
 
     function transfer(address to, uint value) external returns (bool);
+
+    function deposit() external payable;
 
     function withdraw(uint) external;
 }
