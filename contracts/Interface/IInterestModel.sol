cf533e2b6dd86659bb1209b9d7dd7056cf37b1a0
Tao Yafan <tao_yafan@qq.com>
2021-05-10 22:46:00 +0800

    Change view to pure.

--- contracts/Interface/IInterestModel.sol
+++ contracts/Interface/IInterestModel.sol
@@ -1,5 +1,5 @@
 pragma solidity >=0.5.0 <0.8.0;
 
 interface IInterestModel {
-    function getInterestRate(uint256 debt, uint256 floating) external view returns (uint256);
+    function getInterestRate(uint256 debt, uint256 floating) external pure returns (uint256);
 }
