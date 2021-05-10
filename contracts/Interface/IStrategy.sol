6036dfc0cbcc37cff8f2b65b2951785b254479f9
Tao Yafan <tao_yafan@qq.com>
2021-05-10 22:48:57 +0800

    Create IStrategy.sol and Math.sol.

---
+++ contracts/Interface/IStrategy.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.6.0;
+
+interface IStrategy {
+
+    /// @dev Execute worker strategy. Take LP tokens + debt token. Return LP tokens or debt token.
+    /// @param user The original user that is interacting with the operator.
+    /// @param borrowTokens Two borrow token address.
+    /// @param borrows The amount of each borrow token.
+    /// @param debts The user's total debt of each borrow token, for better decision making context.
+    /// @param data Extra calldata information passed along to this strategy.
+    /// return token and amount need transfer back.
+    function execute(
+        address user, 
+        address[2] calldata borrowTokens, 
+        uint256[2] calldata borrows, 
+        uint256[2] calldata debts, 
+        bytes calldata data) external payable;
+
+}
