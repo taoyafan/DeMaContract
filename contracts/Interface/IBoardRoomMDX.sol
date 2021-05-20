b397cdd119f75a1ca9913e3e3baac78ae2f18230
Tao Yafan <tao_yafan@qq.com>
2021-05-20 20:21:41 +0800

    Add board room mdx interface.

---
+++ contracts/Interface/IBoardRoomMDX.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.6.0;
+
+interface IBoardRoomMDX {
+    // User deposited amount and rewardDebt(don't use it).
+    function userInfo(uint _pid, address _user) external view returns (uint256, uint256);
+
+    // Check pending mdx rewards.
+    function pending(uint256 _pid, address _user) external view returns (uint256);
+
+    // Deposit mdx. Note MDX's pid in BSC is 4
+    function deposit(uint256 _pid, uint256 _amount) external;
+
+    // Withdraw both deposited mdx and reward mdx.
+    // if amount is 0 means only withdraw rewards.
+    function withdraw(uint256 _pid, uint256 _amount) external;
+}
