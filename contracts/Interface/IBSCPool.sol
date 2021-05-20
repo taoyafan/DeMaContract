bc43261bef401ab9053da59a9ac62c931416cd28
Tao Yafan <tao_yafan@qq.com>
2021-05-20 20:22:03 +0800

    Add bsc pool interface of pending.

--- contracts/Interface/IBSCPool.sol
+++ contracts/Interface/IBSCPool.sol
@@ -1,6 +1,9 @@
 pragma solidity ^0.6.0;
 
 interface IBSCPool {
+    // Pending mdx rewards and Lp token rewards.
+    function pending(uint256 _pid, address _user) external view returns (uint256, uint256);
+
     function deposit(uint256 pid, uint256 amount) external;
 
     function withdraw(uint256 pid, uint256 amount) external;
