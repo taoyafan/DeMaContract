56e1075c8eb57b20f79615e5123980a0516cbc6e
Yafan Tao <tao_yafan@qq.com>
2021-05-21 18:18:07 +0800

    Add interface getAllRewards in goblin.

--- contracts/Interface/IGoblin.sol
+++ contracts/Interface/IGoblin.sol
@@ -3,6 +3,9 @@ pragma solidity >=0.5.0 <0.8.0;
 
 interface IGoblin {
 
+    /// @dev Send all mdx rewards earned in this goblin to account.
+    function getAllRewards(address account) external;
+
     /**
      * @dev Work on the given position. Must be called by the operator.
      * @param id The position ID to work on.
@@ -15,13 +18,13 @@ interface IGoblin {
      * @param data The encoded data, consisting of strategy address and bytes to strategy.
      */
     function work(
-        uint256 id, 
-        address user, 
+        uint256 id,
+        address user,
         address inviter,
         bool canInvite,
-        address[2] calldata borrowTokens, 
-        uint256[2] calldata borrowAmounts, 
-        uint256[2] calldata debts, 
+        address[2] calldata borrowTokens,
+        uint256[2] calldata borrowAmounts,
+        uint256[2] calldata debts,
         bytes calldata data
     ) external payable;
 
@@ -30,10 +33,10 @@ interface IGoblin {
      * @param id The position ID to perform health check.
      * @param borrowTokens Address of two tokens this position had debt.
      * @param debts Debts of two tokens.
-     */    
+     */
      function health(
-        uint256 id, 
-        address[2] calldata borrowTokens, 
+        uint256 id,
+        address[2] calldata borrowTokens,
         uint256[2] calldata debts
     ) external view returns (uint256[2] memory);
 
@@ -46,9 +49,9 @@ interface IGoblin {
      * @param debts Two tokens debts.
      */
     function liquidate(
-        uint256 id, 
-        address user, 
-        address inviter, 
+        uint256 id,
+        address user,
+        address inviter,
         address[2] calldata borrowTokens,
         uint256[2] calldata debts) external;
 }
