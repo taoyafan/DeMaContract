6eb83118a9d2d4f8c544248d46d81d515a1dc603
Tao Yafan <tao_yafan@qq.com>
2021-05-22 20:43:13 +0800

    Change name of deploy bank.

--- migrations/3_deploy_bank.js
+++ migrations/3_deploy_bank.js
@@ -5,13 +5,13 @@ const BigNumber = require("bignumber.js");
 
 module.exports = function (deployer, network, accounts) {
 
-    deployer.deploy(TripleSlopeModel);
-    deployer.deploy(BankConfig);
-    deployer.deploy(Bank, "0x337610d27c682E347C9cD60BD4b3b107C9d34dDd");
-    if(network == 'development') {
-        const USDT = artifacts.require("ERC20Token");
-        deployer.deploy(USDT, "USDT", "USDT", BigNumber(1000e18));
-    }
+    // deployer.deploy(TripleSlopeModel);
+    // deployer.deploy(BankConfig);
+    // deployer.deploy(Bank, "0x337610d27c682E347C9cD60BD4b3b107C9d34dDd");
+    // if(network == 'development') {
+    //     const USDT = artifacts.require("ERC20Token");
+    //     deployer.deploy(USDT, "USDT", "USDT", BigNumber(1000e18));
+    // }
 
     // deployer.deploy(TripleSlopeModel).then((model) => {
     //     return deployer.deploy(BankConfig);
