ef15ca97c60c354e9d86cc33f350bf6317f2c003
Yafan Tao <tao_yafan@qq.com>
2021-07-05 14:37:37 +0800

    Add some test plans

--- test/test_goblin.js
+++ test/test_goblin.js
@@ -1,7 +1,49 @@
+// Functions
+
+    // (1) Productions and positions open
+        // Only can borrow one token.
+        // Can borrow two.
+
+    // (2) Positions usage
+        // Create
+        // Replenishment
+        // Withdraw
+        // Repay
+
+    // (3) Health and liquidate
+        // Open position health restrict
+        // Health
+        // New health
+        // liquidate health
+
+    // (4) Earn
+        // MDX earn
+        // Farm earn
+
+    // (5) Global and user information
+        // Productions number
+        // Positions number
+        // All positions
+        // Users positions number
+        // Users all positions
+
+    // (6) Invitation gain
+
+
+
+// Test flow:
+    // MDX init
+    // Farm init
+    // Bank init
+    // Add token
+    // Deposit toekn
+    // opProduction
+    // opPosition (Create position)
+    // opPosition (Withdraw position)
 
 
 contract("TestBank", (accounts) => {
-    
+
     // async function lpTest(nameA, nameB, addressA, addressB) {
 
     //     describe("BNB-USDT-LP Production Test", async () => {
@@ -31,4 +73,4 @@ contract("TestBank", (accounts) => {
 
     // describe("BUSD-USDT-LP Test", async () => {
     // });
-}
+})
