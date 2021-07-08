4540f03f2eada6886668ef0c80f1677177f05306
Tao Yafan <tao_yafan@qq.com>
2021-07-08 22:40:49 +0800

    Update reinvestment test.

--- test/TestRewardsPerShare.test.js
+++ test/TestRewardsPerShare.test.js
@@ -16,11 +16,19 @@ contract("TestReinvestment", (accounts) => {
     let mdexBoardRoomPid;
     let depositAmount = BigNumber(1000);
 
-    before("before", async () => {
+    let rewardsPerCycle = BigNumber(1000);
+    let cycles = 50;
+
+    let owner = accounts[0];
+    let user = accounts[1];
+
+    before("Init board room", async () => {
 
         // initiate
         mdxToken = await MdxToken.deployed();
+        mdxToken.transfer(user, depositAmount);
         boardRoom = await BoardRoom.deployed();
+        await boardRoom.setCycle(cycles);
         reinvestment = await Reinvestment.deployed();
         poolLengthBeforeAdd = await boardRoom.poolLength();
     
@@ -34,53 +42,83 @@ contract("TestReinvestment", (accounts) => {
                 await boardRoom.add(1000, testAddress, 0);
                 poolLength = parseInt(await boardRoom.poolLength(), 10);
             }
-
         }
         await boardRoom.add(1000, mdxToken.address, 0);
         poolLength = parseInt(await boardRoom.poolLength(), 10);
 
-        await mdxToken.approve(reinvestment.address, MaxUint256);
-        await reinvestment.deposit(depositAmount);
-        let totalRewardsBefore = await reinvestment.totalRewards();
-        console.log(`totalRewards: ${totalRewardsBefore}`);
-        let globalInfoBefore = await reinvestment.globalInfo();
-        console.log(`globalInfoBefore: ${JSON.stringify(globalInfoBefore)}`);
-        let userInfoBefore = await reinvestment.userInfo(accounts[0]);
-        console.log(`userInfoBefore: ${JSON.stringify(userInfoBefore)}`);
-
+        let blockNum = await web3.eth.getBlockNumber();
+        console.log(`Block number Before new reward ${blockNum}`);
         await mdxToken.approve(boardRoom.address, MaxUint256);
-        await boardRoom.deposit(mdexBoardRoomPid,depositAmount);
+        await boardRoom.newReward(rewardsPerCycle.multipliedBy(cycles), rewardsPerCycle, parseInt(blockNum, 10)+1);
+        // Don't need update pool because it will update automaticly when deposit
+        // await boardRoom.updatePool(mdexBoardRoomPid);    
+
+        let boardRoomStartBlock = await boardRoom.startBlock();
+        let boardRoomEndBlock = await boardRoom.endBlock();
+        console.log(`boardRoomStartBlock :${boardRoomStartBlock}`);
+        console.log(`boardRoomEndBlock :${boardRoomEndBlock}`);
+    })
+
+    it("Deposit and Reinvest", async () => {
+
+        let totalRewardsBefore = await reinvestment.totalRewards();
+        let globalInfoBefore = await reinvestment.globalInfo();
+        let userInfoBefore = await reinvestment.userInfo(user);
+
+        console.log(`totalRewards: ${totalRewardsBefore}`);
+        logObj(globalInfoBefore, 'globalInfoBefore');
+        logObj(userInfoBefore, 'userInfoBefore');
+
+        // Deposit to reinvestment
+        await mdxToken.approve(reinvestment.address, MaxUint256, {from: user});
+        await reinvestment.deposit(depositAmount, {from: user});
+
+        // Reinvest
+        await reinvestment.reinvest();
+        let blockNum = await web3.eth.getBlockNumber();
+        console.log(`Block number when reinvest ${blockNum}`);
+        // await mdxToken.approve(boardRoom.address, MaxUint256, {from: user});
+        // await boardRoom.deposit(mdexBoardRoomPid,depositAmount, {from: user});
+
         let boardRoomMdexAmount = await mdxToken.balanceOf(boardRoom.address);
         let boardMdexPerBlock = await boardRoom.mdxPerBlock();
         let boardPendingResult = await boardRoom.pending(mdexBoardRoomPid, reinvestment.address);
         let boardPoolInfo = await boardRoom.poolInfo(mdexBoardRoomPid);
         let boardUserInfo = await boardRoom.userInfo(mdexBoardRoomPid, reinvestment.address);
+
         console.log(`boardRoomMdexAmount :${boardRoomMdexAmount}`);
         console.log(`boardMdexPerBlock :${boardMdexPerBlock}`);
         console.log(`boardPendingResult :${boardPendingResult}`);
-        console.log(`boardPoolInfo :${JSON.stringify(boardPoolInfo)}`);
-        console.log(`boardUserInfo :${JSON.stringify(boardUserInfo)}`);
-        let boardRoomStartBlock = await boardRoom.startBlock();
-        let boardRoomEndBlock = await boardRoom.endBlock();
-        await boardRoom.newReward(depositAmount, 50, parseInt(boardRoomEndBlock, 10)+1);
-        await boardRoom.updatePool(mdexBoardRoomPid);
-        let boardRoomMdexAmountAfterUpdatePool = await mdxToken.balanceOf(boardRoom.address);
-        let boardMdexPerBlockAfterUpdatePool = await boardRoom.mdxPerBlock();
-        let boardPendingResultAfterUpdatePool = await boardRoom.pending(mdexBoardRoomPid, reinvestment.address);
-        let boardPoolInfoAfterUpdatePool = await boardRoom.poolInfo(mdexBoardRoomPid);
-        let boardUserInfoAfterUpdatePool = await boardRoom.userInfo(mdexBoardRoomPid, reinvestment.address);
-        console.log(`boardRoomMdexAmountAfterUpdatePool :${boardRoomMdexAmountAfterUpdatePool}`);
-        console.log(`boardMdexPerBlockAfterUpdatePool :${boardMdexPerBlockAfterUpdatePool}`);
-        console.log(`boardPendingResultAfterUpdatePool :${boardPendingResultAfterUpdatePool}`);
-        console.log(`boardPoolInfoAfterUpdatePool :${JSON.stringify(boardPoolInfoAfterUpdatePool)}`);
-        console.log(`boardUserInfoAfterUpdatePool :${JSON.stringify(boardUserInfoAfterUpdatePool)}`);
+        logObj(boardPoolInfo, 'boardPoolInfo');
+        logObj(boardUserInfo, 'boardUserInfo');
+
+        // let boardRoomMdexAmountAfterUpdatePool = await mdxToken.balanceOf(boardRoom.address);
+        // let boardMdexPerBlockAfterUpdatePool = await boardRoom.mdxPerBlock();
+        // let boardPendingResultAfterUpdatePool = await boardRoom.pending(mdexBoardRoomPid, reinvestment.address);
+        // let boardPoolInfoAfterUpdatePool = await boardRoom.poolInfo(mdexBoardRoomPid);
+        // let boardUserInfoAfterUpdatePool = await boardRoom.userInfo(mdexBoardRoomPid, reinvestment.address);
+
+        // console.log(`boardRoomMdexAmountAfterUpdatePool :${boardRoomMdexAmountAfterUpdatePool}`);
+        // console.log(`boardMdexPerBlockAfterUpdatePool :${boardMdexPerBlockAfterUpdatePool}`);
+        // console.log(`boardPendingResultAfterUpdatePool :${boardPendingResultAfterUpdatePool}`);
+        // console.log(`boardPoolInfoAfterUpdatePool :${JSON.stringify(boardPoolInfoAfterUpdatePool)}`);
+        // console.log(`boardUserInfoAfterUpdatePool :${JSON.stringify(boardUserInfoAfterUpdatePool)}`);
 
         let totalRewardsAfterUpdatePool = await reinvestment.totalRewards();
+        let userRewardsAfterUpdatePool = await reinvestment.userEarnedAmount(user);
+        let ownerRewardsAfterUpdatePool = await reinvestment.userEarnedAmount(owner);
+
         console.log(`totalRewardsAfterUpdatePool: ${totalRewardsAfterUpdatePool}`);
-        let globalInfoAfterUpdatePool = await reinvestment.globalInfo();
-        console.log(`globalInfoAfterUpdatePool: ${JSON.stringify(globalInfoAfterUpdatePool)}`);
-        let userInfoAfterUpdatePool = await reinvestment.userInfo(accounts[0]);
-        console.log(`userInfoAfterUpdatePool: ${JSON.stringify(userInfoAfterUpdatePool)}`);
+        console.log(`userRewardsAfterUpdatePool: ${userRewardsAfterUpdatePool}`);
+        console.log(`ownerRewardsAfterUpdatePool: ${ownerRewardsAfterUpdatePool}`);
+        
+        let blockNumWhenGetRewards = await web3.eth.getBlockNumber();
+        console.log(`Block number when get total rewards ${blockNumWhenGetRewards}`);
+
+        // let globalInfoAfterUpdatePool = await reinvestment.globalInfo();
+        // let userInfoAfterUpdatePool = await reinvestment.userInfo(accounts[0]);
+        // console.log(`globalInfoAfterUpdatePool: ${JSON.stringify(globalInfoAfterUpdatePool)}`);
+        // console.log(`userInfoAfterUpdatePool: ${JSON.stringify(userInfoAfterUpdatePool)}`);
 
     });
 
@@ -88,8 +126,6 @@ contract("TestReinvestment", (accounts) => {
         let rewardsPerShareAmount = await reinvestment.rewardsPerShare();
         assert.equal(rewardsPerShareAmount.toNumber(), 0);
     });
-
-
 })
 
 function toWei(ether) {
@@ -98,4 +134,15 @@ function toWei(ether) {
 
 function fromWei(wei) {
     return web3.utils.fromWei(BigNumber(wei).toString())
+}
+
+function logObj(obj, name) {
+    console.log(` ------------------ ${name}: ------------------ `)
+    for (let key in obj) {
+        // Not log number
+        if (parseFloat(key).toString() == "NaN") {
+            console.log(key + ": " + obj[key].toString());
+        }
+    }
+    console.log()
 }
