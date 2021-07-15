460f0431cd85aad72f16f4da1a678ff9096b75b1
Tao Yafan <tao_yafan@qq.com>
2021-07-15 23:10:58 +0800

    Add goblin deploy.

--- migrations/5_deploy_goblin.js
+++ migrations/5_deploy_goblin.js
@@ -3,10 +3,47 @@ const MdxStrategyWithdrawMinimizeTrading = artifacts.require("MdxStrategyWithdra
 const MdxGoblin = artifacts.require("MdxGoblin");
 const Reinvestment = artifacts.require("Reinvestment");
 const Farm = artifacts.require("Farm");
+const BSCPool = artifacts.require("BSCPool");
+const MdexRouter = artifacts.require("MdexRouter");
+const MdxToken = artifacts.require("MdxToken");
 
 const BigNumber = require("bignumber.js");
+const fs = require('fs')
 
 module.exports = async function (deployer, network, accounts) {
 
+    const jsonString = fs.readFileSync("bin/contracts/address.json")
+    const addressJson = JSON.parse(jsonString)
     
+    const MdxGoblinBnbUsdt = await deployer.deploy(
+        MdxGoblin, 
+        Farm.address,
+        100,                    // Farm pool id, Goblin begin from 100
+        Reinvestment.address,
+        BSCPool.address,
+        4,                      // Bsc pool id
+        MdexRouter.address,
+        MdxToken.address,
+        "0x0000000000000000000000000000000000000000",
+        addressJson.USDT,
+        MdxStrategyWithdrawMinimizeTrading.address
+    );
+    
+    const MdxGoblinMdxUsdt = await deployer.deploy(
+        MdxGoblin, 
+        Farm.address,
+        101,                    // Farm pool id, Goblin begin from 100
+        Reinvestment.address,
+        BSCPool.address,
+        4,                      // Bsc pool id
+        MdexRouter.address,
+        MdxToken.address,
+        MdxToken.address,
+        addressJson.USDT,
+        MdxStrategyWithdrawMinimizeTrading.address
+    );
+
+
+    saveToJson("MdxGoblinBnbUsdt", MdxGoblinBnbUsdt.address);
+    saveToJson("MdxGoblinMdxUsdt", MdxGoblinMdxUsdt);
 };
