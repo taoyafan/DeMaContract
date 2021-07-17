a7da774e7e56d35baa35dec2deac094ab302a317
Tao Yafan <tao_yafan@qq.com>
2021-07-17 18:44:40 +0800

    Add goblin deploy and move reinvest inside.

--- migrations/5_deploy_goblin.js
+++ migrations/5_deploy_goblin.js
@@ -1,46 +1,64 @@
 const MdxStrategyWithdrawMinimizeTrading = artifacts.require("MdxStrategyWithdrawMinimizeTrading");
+const MdxStrategyAddTwoSidesOptimal = artifacts.require("MdxStrategyAddTwoSidesOptimal");
 const MdxGoblin = artifacts.require("MdxGoblin");
 const Reinvestment = artifacts.require("Reinvestment");
-const Farm = artifacts.require("Farm");
-const BSCPool = artifacts.require("BSCPool");
-const MdexRouter = artifacts.require("MdexRouter");
-const MdxToken = artifacts.require("MdxToken");
 
+const { assert } = require('console');
 const fs = require('fs')
+let saveToJson = require('./save_address_to_json.js')
 
 module.exports = async function (deployer, network, accounts) {
 
     const jsonString = fs.readFileSync("bin/contracts/address.json")
     const addressJson = JSON.parse(jsonString)
 
-    const MdxGoblinBnbUsdt = await deployer.deploy(
-        MdxGoblin,
-        Farm.address,
-        100,                    // Farm pool id, Goblin begin from 100
-        Reinvestment.address,
-        BSCPool.address,
-        4,                      // Bsc pool id
-        MdexRouter.address,
-        MdxToken.address,
-        "0x0000000000000000000000000000000000000000",   // Token0   bnb
-        addressJson.USDT,                               // Token1   usdt
-        MdxStrategyWithdrawMinimizeTrading.address
-    );
+    // TODO found the right mdx pool id.
+    assert(network == 'development')
 
-    const MdxGoblinMdxUsdt = await deployer.deploy(
-        MdxGoblin,
-        Farm.address,
-        101,                    // Farm pool id, Goblin begin from 100
-        Reinvestment.address,
-        BSCPool.address,
-        4,                      // Bsc pool id
-        MdexRouter.address,
-        MdxToken.address,
-        MdxToken.address,                               // Token0   mdx
-        addressJson.USDT,                               // Token1   usdt
-        MdxStrategyWithdrawMinimizeTrading.address
-    );
+    productions = [
+        {
+            token0: "Bnb", 
+            token1: "Busd", 
+            token0Address: addressJson.WBNB,
+            token1Address: addressJson.BUSD, 
+            farmPoolId: 100,    // Goblin begin from 100
+            mdxPoolId: 0,
+        },
+        {
+            token0: "Mdx", 
+            token1: "Busd", 
+            token0Address: addressJson.MdxToken,
+            token1Address: addressJson.BUSD, 
+            farmPoolId: 101,
+            mdxPoolId: 1,
+        },
+    ];
 
-    saveToJson("MdxGoblinBnbUsdt", MdxGoblinBnbUsdt.address);
-    saveToJson("MdxGoblinMdxUsdt", MdxGoblinMdxUsdt);
+    await deployer.deploy(Reinvestment, addressJson.BoardRoomMDX, 4, addressJson.MdxToken, 1000);
+    await deployer.deploy(MdxStrategyWithdrawMinimizeTrading, addressJson.MdexRouter);
+    await deployer.deploy(MdxStrategyAddTwoSidesOptimal, addressJson.MdexRouter);
+
+    saveToJson(`Reinvestment`, Reinvestment.address);
+    saveToJson(`MdxStrategyWithdrawMinimizeTrading`, MdxStrategyWithdrawMinimizeTrading.address);
+    saveToJson(`MdxStrategyAddTwoSidesOptimal`, MdxStrategyAddTwoSidesOptimal.address);
+
+    for (prod of productions) {
+        prod.goblin = await deployer.deploy(
+            MdxGoblin,
+            addressJson.Bank,
+            addressJson.Farm,
+            prod.farmPoolId,                     // Farm pool id, Goblin begin from 100
+            Reinvestment.address,
+            addressJson.BSCPool,
+            prod.mdxPoolId,                      // Bsc pool id
+            addressJson.MdexRouter,
+            addressJson.MdxToken,
+            prod.token0Address,                  // Token0 address
+            prod.token1Address,                  // Token1 address
+            MdxStrategyWithdrawMinimizeTrading.address
+        );
+
+        saveToJson(`MdxGoblin${prod.token0}${prod.token1}`, prod.goblin.address);
+        saveToJson(`Mdx${prod.token0}${prod.token1}FarmPoolId`, prod.farmPoolId);
+    }
 };
--- migrations/6_deploy_reinvestment.js
+++ migrations/6_deploy_reinvestment.js
@@ -5,7 +5,7 @@ const Reinvestment = artifacts.require("Reinvestment");
 const BigNumber = require("bignumber.js");
 
 module.exports = async function (deployer, network, accounts) {
-    await deployer.deploy(MdxToken);
-    await deployer.deploy(BoardRoom, MdxToken.address, 1);
-    await deployer.deploy(Reinvestment, BoardRoom.address, 4, MdxToken.address, 1000);
+    // await deployer.deploy(MdxToken);
+    // await deployer.deploy(BoardRoom, MdxToken.address, 1);
+    // await deployer.deploy(Reinvestment, BoardRoom.address, 4, MdxToken.address, 1000);
 };
