12c0b5f42c70c1a897b01d8f053cf50954959bce
Tao Yafan <tao_yafan@qq.com>
2021-09-12 10:29:34 +0800

    Update mdx init to add liquidate

--- scripts/mdx_init.js
+++ scripts/mdx_init.js
@@ -1,37 +1,70 @@
-'use strict'
-
 const MdexFactory = artifacts.require("MdexFactory");
 const WBNB = artifacts.require("WBNB");
-const BUSD = artifacts.require("ERC20Token");
-const MdexRouter = artifacts.require("MdexRouter");
+const ERC20Token = artifacts.require("ERC20Token");
 const MdexPair = artifacts.require("MdexPair");
-const MdxToken = artifacts.require("MdxToken");
+
+const BigNumber = require("bignumber.js");
+BigNumber.config({ EXPONENTIAL_AT: 30 })
+
+const { assert } = require('console');
+let {saveToJson, readAddressJson} = require('../js_utils/jsonRW.js');
+let {getProdInfo} = require('../js_utils/config.js');
+const bnbAddress = '0x0000000000000000000000000000000000000000'
+
+const addressJson = readAddressJson('bsctest')
 
 function mdxInit(callback) {
 
     async function fun() {
+        const networkId = await web3.eth.net.getId();
+        assert(networkId == 97)
+        const accounts = await web3.eth.getAccounts();
+        await addLiquidate(bnbAddress, addressJson.USDT, BigNumber(5e18), BigNumber(2000e18), accounts[0]);
+        await addLiquidate(addressJson.BTC, addressJson.USDT, BigNumber(10e18), BigNumber(500000e18), accounts[0]);
+        await addLiquidate(addressJson.ETH, addressJson.USDT, BigNumber(100e18), BigNumber(400000e18), accounts[0]);
+    }
 
-        let factory = await MdexFactory.deployed();
-        let wbnb = await WBNB.deployed();
-        let busd = await BUSD.deployed();
-        let router = await MdexRouter.deployed();
-        let mdx = await MdxToken.deployed();
+    fun().then(callback);
+}
 
-        // Get pair
-        let lpAddresss = await factory.getPair(wbnb.address, busd.address);
-        let wbnb_busd_lp = await MdexPair.at(lpAddresss);
-        
-        lpAddresss = await factory.getPair(wbnb.address, busd.address);
-        let mdx_busd_lp = await MdexPair.at(lpAddresss);
+async function addLiquidate(token0, token1, r0, r1, from) {
+    let wbnb = await WBNB.at(addressJson.WBNB);
+    if (token0 == bnbAddress) {
+        token0 = addressJson.WBNB
+        await wbnb.deposit({from: from, value: r0})
+    } else if (token1 == bnbAddress) {
+        token1 = addressJson.WBNB
+        await wbnb.deposit({from: from, value: r1})
+    }
 
-        if(callback) {
-            callback();
-        }
+    // let router = await MdexRouter.at(addressJson.MdexRouter);
+    // await approve(token0, router.address, r0, from)
+    // await approve(token1, router.address, r1, from)
 
-        return [factory, wbnb, busd, router, wbnb_busd_lp, mdx, mdx_busd_lp];
-    };
+    let factory = await MdexFactory.at(addressJson.MdexFactory);
+    let lpAddress = await factory.getPair(token0, token1);
+    let lp = await MdexPair.at(lpAddress)
+    await transfer(token0, lpAddress, r0, from)
+    await transfer(token1, lpAddress, r1, from)
+    await lp.mint(from)
+}
 
-    return fun();
+
+async function approve(tokenAddress, to, amount, from) {
+    if (tokenAddress == bnbAddress)
+        return
+
+    let token = await ERC20Token.at(tokenAddress);
+    await token.approve(to, amount, {from: from});
+}
+
+async function transfer(tokenAddress, to, amount, from) {
+    if (tokenAddress == bnbAddress) {
+        await web3.eth.sendTransaction({from: from, to: to, value: amount})
+    } else {
+        let token = await ERC20Token.at(tokenAddress);
+        await token.transfer(to, amount, {from: from});
+    }
 }
 
 module.exports = mdxInit;
