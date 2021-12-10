39804242310bfc2e87d482024709e02d29c9fe95
Tao Yafan <tao_yafan@qq.com>
2021-12-10 21:46:43 +0800

    Update bsctest RPC

--- truffle-config.js
+++ truffle-config.js
@@ -74,7 +74,7 @@ module.exports = {
     // },
     // Useful for private networks
     bsctest: {
-      provider: () => new HDWalletProvider(privateKey, "https://data-seed-prebsc-2-s3.binance.org:8545/"),
+      provider: () => new HDWalletProvider(privateKey, "https://data-seed-prebsc-1-s1.binance.org:8545"),
       network_id: 97,
       gas: 8000000,           // Gas sent with each transaction (default: ~6700000)
       gasPrice: 20000000000,   // 20 gwei (in wei)
