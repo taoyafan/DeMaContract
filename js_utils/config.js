c79a49a237b5aa3d482136da9275891e16af16dd
Yafan Tao <tao_yafan@qq.com>
2021-12-29 14:10:11 +0800

    Finished part of cake migrate

--- js_utils/config.js
+++ js_utils/config.js
@@ -1,11 +1,11 @@
-let {readAddressJson} = require('./jsonRW.js');
+let {readAddressJson} = require("./jsonRW.js");
 const BigNumber = require("bignumber.js");
 
 function getBanksInfo(network) {
     let bankInfo;
     let addressJson = readAddressJson(network);
 
-    if (network == 'development') {
+    if (network == "development") {
         bankInfo = [
             {
                 token: "Bnb",
@@ -28,7 +28,7 @@ function getBanksInfo(network) {
                 rewardFirstPeriod: BigNumber(18050).multipliedBy(1e18)
             }
         ]
-    } else if (network == 'bsctest') {
+    } else if (network == "bsctest") {
         bankInfo = [
             {
                 token: "Busd",
@@ -57,75 +57,83 @@ function getBanksInfo(network) {
             },
         ]
     } else {
-        throw new Error('Network not support');
+        throw new Error("Network not support");
     }
     return bankInfo
 }
 
-function getProdInfo(network) {
+function getProdInfo(network, dex="Mdx") {
     let productions;
     let addressJson = readAddressJson(network);
 
     // TODO found the right mdx pool id. and rewardFirstPeriod
-    if (network == 'development') {
-        productions = [
-            {
-                token0: "Bnb", 
-                token1: "Busd", 
-                token0Address: "0x0000000000000000000000000000000000000000",
-                token1Address: addressJson.BUSD, 
-                rewardFirstPeriod: BigNumber(60*60*24*30).multipliedBy(1e18),    // 1 DEMA per second.
-            },
-            {
-                token0: "Mdx", 
-                token1: "Busd", 
-                token0Address: addressJson.MdxToken,
-                token1Address: addressJson.BUSD, 
-                rewardFirstPeriod: BigNumber(60*60*24*30).multipliedBy(1e18),    // 1 DEMA per second.
-            },
-            {
-                token0: "Usdt", 
-                token1: "Busd", 
-                token0Address: addressJson.USDT,
-                token1Address: addressJson.BUSD, 
-                rewardFirstPeriod: BigNumber(60*60*24*30).multipliedBy(1e18),    // 1 DEMA per second.
-            },
-        ];
-    } else if (network == 'bsctest') {
-        productions = [
-            {
-                token0: "Bnb", 
-                token1: "Usdt", 
-                token0Address: "0x0000000000000000000000000000000000000000",
-                token1Address: addressJson.USDT, 
-                rewardFirstPeriod: BigNumber(3610).multipliedBy(1e18),    // 1 DEMA per second.
-                r0: BigNumber(10e18),
-                r1: BigNumber(3000e18),        // 300
-            },
-            {
-                token0: "Btc", 
-                token1: "Usdt", 
-                token0Address: addressJson.BTC,
-                token1Address: addressJson.USDT, 
-                rewardFirstPeriod: BigNumber(3610).multipliedBy(1e18),    // 1 DEMA per second.
-                r0: BigNumber(1000e18),
-                r1: BigNumber(60000000e18),    // 6 W
-            },
-            {
-                token0: "Eth", 
-                token1: "Usdt", 
-                token0Address: addressJson.ETH,
-                token1Address: addressJson.USDT, 
-                rewardFirstPeriod: BigNumber(3610).multipliedBy(1e18),    // 1 DEMA per second.
-                r0: BigNumber(1000e18),
-                r1: BigNumber(4000000e18),     // 4000
-            },
-        ];
+    if (network == "development") {
+        productions = {
+            "Mdx": [
+                {
+                    token0: "Bnb",
+                    token1: "Busd",
+                    token0Address: "0x0000000000000000000000000000000000000000",
+                    token1Address: addressJson.BUSD,
+                    rewardFirstPeriod: BigNumber(60*60*24*30).multipliedBy(1e18),    // 1 DEMA per second.
+                },
+                {
+                    token0: "Mdx",
+                    token1: "Busd",
+                    token0Address: addressJson.MdxToken,
+                    token1Address: addressJson.BUSD,
+                    rewardFirstPeriod: BigNumber(60*60*24*30).multipliedBy(1e18),    // 1 DEMA per second.
+                },
+                {
+                    token0: "Usdt",
+                    token1: "Busd",
+                    token0Address: addressJson.USDT,
+                    token1Address: addressJson.BUSD,
+                    rewardFirstPeriod: BigNumber(60*60*24*30).multipliedBy(1e18),    // 1 DEMA per second.
+                },
+            ],
+        }
+        productions["Cake"] = productions["Mdx"]
+    } else if (network == "bsctest") {
+        productions = {
+            "Mdx": [
+                {
+                    token0: "Bnb",
+                    token1: "Usdt",
+                    token0Address: "0x0000000000000000000000000000000000000000",
+                    token1Address: addressJson.USDT,
+                    rewardFirstPeriod: BigNumber(3610).multipliedBy(1e18),    // 1 DEMA per second.
+                    r0: BigNumber(10e18),
+                    r1: BigNumber(3000e18),        // 300
+                },
+                {
+                    token0: "Btc",
+                    token1: "Usdt",
+                    token0Address: addressJson.BTC,
+                    token1Address: addressJson.USDT,
+                    rewardFirstPeriod: BigNumber(3610).multipliedBy(1e18),    // 1 DEMA per second.
+                    r0: BigNumber(1000e18),
+                    r1: BigNumber(60000000e18),    // 6 W
+                },
+                {
+                    token0: "Eth",
+                    token1: "Usdt",
+                    token0Address: addressJson.ETH,
+                    token1Address: addressJson.USDT,
+                    rewardFirstPeriod: BigNumber(3610).multipliedBy(1e18),    // 1 DEMA per second.
+                    r0: BigNumber(1000e18),
+                    r1: BigNumber(4000000e18),     // 4000
+                },
+            ],
+            "Cake": [
+            ]
+        }
     } else {
-        throw new Error('Network not support');
+        throw new Error("Network not support");
     }
 
-    return productions
+    console.assert(dex in productions, "Dex not support");
+    return productions[dex]
 }
 
 module.exports = {
--- migrations/4_deploy_cake.js
+++ migrations/4_deploy_cake.js
@@ -1,6 +1,9 @@
 const WBNB = artifacts.require("WBNB");
-const MdexRouter = artifacts.require("PancakeFactory");
-const MdexRouter = artifacts.require("PancakeRouter");
+const CakeToken = artifacts.require("CakeToken");
+const SyrupBar = artifacts.require("SyrupBar");
+const CakeFactory = artifacts.require("PancakeFactory");
+const CakeRouter = artifacts.require("PancakeRouter");
+const MasterChef = artifacts.require("MasterChef");
 
 const BigNumber = require("bignumber.js");
 let {saveToJson, readAddressJson} = require('../js_utils/jsonRW.js');
@@ -16,100 +19,61 @@ const {
 } = require("../js_utils/utils");
 
 module.exports = async function (deployer, network, accounts) {
-    
+
     if (network == 'development' || network == 'bsctest') {
         const { addressJson } = setNetwork(network, web3);
         const busdAddress = addressJson.BUSD;
 
-        await deployer.deploy(MdxToken)      // Mdex Token
-        saveToJson("MdxToken", MdxToken.address, network);
+        let cakeToken = await deployer.deploy(CakeToken)      // Cake Token
+        saveToJson("CakeToken", CakeToken.address, network);
 
         await deployer.deploy(
-            MdexFactory,            // Factory
+            CakeFactory,            // Factory
             accounts[0]
         );
-        saveToJson("MdexFactory", MdexFactory.address, network);
-
-        // WBNB should be saved in addressJson after deploying mdx.
-        let wbnb = await WBNB.at(addressJson.WBNB)
+        saveToJson("CakeFactory", CakeFactory.address, network);
 
         let router = await deployer.deploy(
-            MdexRouter,             // MdexRouter
-            MdexFactory.address,
-            wbnb.address
+            CakeRouter,             // CakeRouter
+            CakeFactory.address,
+            addressJson.WBNB        // WBNB should be saved in addressJson after deploying cake.
         );
-        saveToJson("MdexRouter", router.address, network);
 
         await deployer.deploy(
-            Oracle,                 // Oracle
-            MdexFactory.address
+            SyrupBar,                // MasterChef
+            CakeToken.address,
         );
-        saveToJson("Oracle", Oracle.address, network);
 
-        await deployer.deploy(
-            SwapMining,             // SwapMining
-            MdxToken.address,
-            MdexFactory.address,
-            Oracle.address,
-            MdexRouter.address,
-            busdAddress,
-            BigNumber(1e18),    //_mdxPerBlock,
-            0                   // startBlock
-        );
-        saveToJson("SwapMining", SwapMining.address, network);
-        
-        // Set swap ming for router, TODO SwapMining need to config.
-        await router.setSwapMining(SwapMining.address);
-
-        await deployer.deploy(
-            BSCPool,                // BSCPool
-            MdxToken.address,
-            BigNumber(1e18),        //_mdxPerBlock, 1 mdx per block
+        let masterChef = await deployer.deploy(
+            MasterChef,             // MasterChef
+            CakeToken.address,
+            SyrupBar.address,
+            accounts[0],
+            BigNumber(1e18),        // cakePerBlock, 1 cake per block
             0                       // startBlock
         );
-        saveToJson("BSCPool", BSCPool.address, network);
+        saveToJson("MasterChef", MasterChef.address, network);
 
-        let rewardsPerCycle = BigNumber(1e17);
-        let cycles = 1000;
-        let boardRoom = await deployer.deploy(
-            BoardRoomMDX,
-            MdxToken.address,
-            cycles       // how many
-        );
-        saveToJson("BoardRoomMDX", BoardRoomMDX.address, network);
+        // MasterChef related operations
 
-        // BoardRoom add pool
-        let mdxToken = await MdxToken.deployed();
-        let blockNum = await web3.eth.getBlockNumber();
-        await mdxToken.approve(boardRoom.address, MaxUint256);
-        await boardRoom.newReward(rewardsPerCycle.multipliedBy(cycles), rewardsPerCycle, +blockNum+1);
+        // - Transfer owner ship
+        cake.transferOwnership(MasterChef.address);
 
-        await boardRoom.add(0, mdxToken.address, false);
-        await boardRoom.add(0, mdxToken.address, false);
-        await boardRoom.add(0, mdxToken.address, false);
-        await boardRoom.add(0, mdxToken.address, false);
-        await boardRoom.add(1000, mdxToken.address, false);     // MDX pool
+        // - Initialize each lp pool
+        let productions = getProdInfo(network, "Cake");
+        let poolId = 1;
 
-        // BSC pool related operations
-        // - Add minter to BSC pool
-        let mdx = await MdxToken.deployed();
-        await mdx.addMinter(BSCPool.address);
-
-        // Initialize each lp pool
-        let bscPool = await BSCPool.deployed();
-        let productions = getProdInfo(network);
-        let poolId = 0;
-        
-        // Update addressJson in utils 
+        // TODO unfinished
+        // - Update addressJson in utils
         setNetwork(network, web3);
-        
+
         for (prod of productions) {
             console.log(`Begin to config ${prod.token0} and ${prod.token1} of account ${accounts[0]}`);
 
             let lp = await createPair(prod.token0Address, prod.token1Address);
             await bscPool.add(1000, lp, false);
-                
-            saveToJson(`Mdx${prod.token0}${prod.token1}PoolId`, poolId++, network);
+
+            saveToJson(`Cake${prod.token0}${prod.token1}PoolId`, poolId++, network);
 
             if (network == 'bsctest') {
                 await addLiquidate(prod.token0Address, prod.token1Address, prod.r0, prod.r1, accounts[0]);
